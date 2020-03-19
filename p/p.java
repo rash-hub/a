d6929f292763a641edb055f1d18ce23bbbc2c250
praveencholarajan <praveen@ecphasisinfotech.com>
2020-03-19 14:24:33 -0400

    c

--- p/p.java
+++ p/p.java
@@ -1,9 +1,23 @@
 package p;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import aa.repository;
+
 public class p {
 public static void main(String[] args) {
 	int a=12;
 	int b=10
 	System.out.println(a+b);
+	@Test(dataProvider="data",dataProviderClass=repository.class)
+	public void test1(String email,String password)  {
+	repository r=new repository();
+	click(r.getSignin());
+	sendkeys(r.getEmail(),email);
+	sendkeys(r.getPassword(),password);
+	click(r.getLogin());
+	Assert.assertTrue(r.getRasheed().isDisplayed());
+	System.out.println("end");
 }
 }
